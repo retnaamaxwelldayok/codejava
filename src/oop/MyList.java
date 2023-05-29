118234379557f3cf6cc1f7238466dd43b953d963
retnaamaxwelldayok <retnaamaxwell@gmain.com>
2023-05-30 00:03:19 +0100

    Updated behaviours of the List

--- src/oop/MyList.java
+++ src/oop/MyList.java
@@ -1,5 +1,7 @@
 package oop;
 
+import java.util.Arrays;
+
 public class MyList {
 
     private final int DEFAULT_LIST_SIZE = 10;
@@ -14,16 +16,21 @@ public class MyList {
     }
 
     public void add(int index, int element) {
-        myList[index] = element;
+        validateIndex(index);
+        boolean indexEqualsTopOfList = listSize == index;
+        if(indexEqualsTopOfList) {
+            doubleMyListSizeAndAddElement(element);
+        } else { insertElementAtGivenIndex(index, element);}
         listSize++;
         isEmpty = false;
     }
 
-    public void remove(int index) {
-        for (int count = index; count < listSize; count ++)
-            myList[count] = myList[index + 1];
-        listSize--;
-        if (listSize == 0) isEmptyIsTrue();
+    public void add(int element) {
+        boolean indexEqualsTopOfList = listSize == myList.length;
+        if(indexEqualsTopOfList) doubleMyListSizeAndAddElement(element);
+        myList[listSize] = element;
+        listSize++;
+        isEmpty = false;
     }
 
     private void isEmptyIsTrue() {
@@ -43,4 +50,57 @@ public class MyList {
             myList[index] = 0;
         listSize = 0;
     }
+
+    private void insertElementAtGivenIndex(int index, int element) {
+        int[] myListCopy = new int[DEFAULT_LIST_SIZE];
+        int counter2 = 0;
+        for (int counter = 0; counter < listSize + 1; counter++){
+            if (counter == index){
+                myListCopy[counter2] = element;
+                counter2++;
+                myListCopy[counter2] = myList[counter];
+            }
+            myListCopy[counter2] = myList[counter];
+            counter2++;
+        }
+        System.arraycopy(myListCopy, 0, myList, 0, (listSize + 1));
+    }
+
+    private void doubleMyListSizeAndAddElement(int element) {
+        int[] myListCopy = new int[listSize * 2];
+        System.arraycopy(myList, 0, myListCopy, 0, listSize);
+        myListCopy[listSize] = element;
+        myList = new int[listSize * 2];
+        System.arraycopy(myListCopy, 0, myList, 0, ++listSize);
+    }
+
+    private void validateIndex(int index) {
+        boolean invalidIndex = index < 0 || index > listSize;
+        if (invalidIndex) throw new ArrayIndexOutOfBoundsException(" Index "+ index +" is not af bound for List");
+    }
+
+    public void remove(int index) {
+        validateIndex(index);
+        for (int count = index; count < listSize; count ++)
+            myList[count] = myList[index + 1];
+        listSize--;
+        myList[listSize] = 0;
+        if (listSize == 0) isEmptyIsTrue();
+    }
+
+    public int indexOf(int element) {
+        int index = 0;
+        for (; index < listSize; index++)
+            if (myList[index] == element) break;
+        return index;
+    }
+
+    public int lastIndexOf(int element) {
+        int index = 0;
+        int elementLastIndex = 0;
+        for (; index < listSize; index++)
+            if (myList[index] == element) elementLastIndex = index;
+        return elementLastIndex;
+    }
 }
+
--- test/oop/MyListTest.java
+++ test/oop/MyListTest.java
@@ -10,6 +10,7 @@ public class MyListTest {
     @BeforeEach public void beforeEach(){
         myList = new MyList();
     }
+
     @Test public void listExistTest(){
 
         assertNotNull(myList);
@@ -22,22 +23,23 @@ public class MyListTest {
 
     @Test public void addElementToList_isEmptyReturnsFalseTest(){
 
-        myList.add(0, 3);
+        myList.add(3);
 
         assertFalse(myList.isEmpty());
     }
 
     @Test public void addElement3And2_Remove3And_isEmptyReturnsFalseTest(){
-        myList.add(0, 3);
-        myList.add(1, 4);
+        myList.add(3);
+        myList.add(4);
 
         myList.remove(1);
 
         assertFalse(myList.isEmpty());
     }
+
     @Test public void addElement3And4_RemoveAllElements_isEmptyShouldReturnTrueTest(){
-        myList.add(0, 3);
-        myList.add(1, 4);
+        myList.add(3);
+        myList.add(4);
 
         myList.remove(0);
         myList.remove(1);
@@ -45,55 +47,123 @@ public class MyListTest {
         assertTrue(myList.isEmpty());
     }
 
+    @Test public void throwExceptionIfIndexGivenForRemoveMethodIsOutOfBoundsTest(){
+        myList.add(3);
+        myList.add(4);
+        myList.add(4);
+
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> {
+            myList.remove(5);
+        });
+    }
+
     @Test public void addElementsToListAndGetElementAtIndex2Test(){
-        myList.add(0, 3);
-        myList.add(1, 4);
-        myList.add(2, 5);
-        myList.add(3, 5);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(5);
 
         int expectedElement = myList.get(2);
 
         assertEquals(5, expectedElement);
     }
+
     @Test public void addElementsToListAndGetElementAtIndex1Test(){
-        myList.add(0, 3);
-        myList.add(1, 4);
-        myList.add(2, 5);
-        myList.add(3, 5);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(5);
 
         int expectedElement = myList.get(1);
 
         assertEquals(4, expectedElement);
     }
+
     @Test public void sizeOfListTest(){
-        myList.add(0, 3);
-        myList.add(1, 4);
-        myList.add(2, 5);
-        myList.add(3, 5);
+        myList.add(3);
+        myList.add(14);
+        myList.add(5);
+        myList.add(5);
 
         assertEquals(4, myList.size());
     }
+
     @Test public void afterAddElements_clearShouldMakeListEmpty(){
-        myList.add(0, 3);
-        myList.add(1, 4);
-        myList.add(2, 5);
-        myList.add(3, 5);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(5);
 
         myList.clear();
 
         assertTrue(myList.isEmpty());
     }
+
     @Test public void afterClearingList_getShouldReturn_0_Test(){
-        myList.add(0, 3);
-        myList.add(1, 4);
-        myList.add(2, 5);
-        myList.add(3, 5);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(5);
 
         myList.clear();
 
+        assertNotNull(myList);
         assertEquals(0, myList.get(2));
     }
 
+    @Test public void overloadedAddMethodThatAddElementGivenAtTheEndOfListTest(){
+        myList.add(5);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(8);
+        myList.add(3);
+        myList.add(4);
 
+        myList.add(20);
+
+        assertEquals(20, myList.get(7));
+    }
+
+    @Test public void addElementInTheMiddleOfAListTest(){
+        myList.add(5);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(8);
+        myList.add(3);
+
+        myList.add(3, 20);
+
+        assertEquals(7, myList.size());
+        assertEquals(20, myList.get(3));
+        assertEquals(5, myList.get(4));
+        assertEquals(8, myList.get(5));
+        assertEquals(3, myList.get(6));
+    }
+    @Test public void methodIndexOfThatReturnsTheIndexOfElementsFirstOccurrenceTest(){
+        myList.add(5);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(8);
+        myList.add(3);
+
+        int indexOf = myList.indexOf(3);
+
+        assertEquals(1, indexOf);
+    }
+    @Test public void methodIndexOfThatReturnsTheIndexOfElementsLastOccurrenceTest(){
+        myList.add(5);
+        myList.add(3);
+        myList.add(4);
+        myList.add(5);
+        myList.add(8);
+        myList.add(3);
+
+        int indexOf = myList.lastIndexOf(3);
+
+        assertEquals(5, myList.lastIndexOf(3));
+    }
 
 }
