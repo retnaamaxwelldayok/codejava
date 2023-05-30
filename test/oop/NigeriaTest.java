91b497bb6cca058264c96090365311162c773a2b
retnaamaxwelldayok <retnaamaxwell@gmain.com>
2023-05-30 10:25:33 +0100

    Started testing the Nigeria app

---
+++ test/oop/NigeriaTest.java
@@ -0,0 +1,13 @@
+package oop;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class NigeriaTest {
+    Nigeria nija = new Nigeria();
+    @Test public void assertObjectIsNotNull(){
+        assertNotNull(nija);
+    }
+
+}
